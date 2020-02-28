c14a6a871adc243c8568d23abb4c03a63f25130d
ardevelopertt <argamert@gmx.de>
2020-02-28 09:38:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import Button from '@material-ui/core/Button';
+import Start from './components/Start';
+import About from './components/About';
+import Whitepaper from './components/Whitepaper';
+import Roadmap from './components/Roadmap';
+import Contribute from './components/Contribute';
+import Team from './components/Team';
+import scrollToComponent from 'react-scroll-to-component';
 
 class App extends Component {
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+      <div>
+        <nav>
+
+          <a href="/" class="titleICO">
+            <i class="material-icons">group_work</i>
+            <div>DEMOICO</div>
           </a>
-        </header>
+
+          <div class="middleNav">
+            <a onClick={() => scrollToComponent(this.About, { offset: -70, align: 'top', duration: 1500})}><Button>About</Button></a>
+            <a onClick={() => scrollToComponent(this.Whitepaper, { offset: -70, align: 'top', duration: 1500})}><Button>Whitepaper</Button></a>
+            <a onClick={() => scrollToComponent(this.Roadmap, { offset: -70, align: 'top', duration: 1500})}><Button>Roadmap</Button></a>
+            <a onClick={() => scrollToComponent(this.Contribute, { offset: -70, align: 'top', duration: 1500})}><Button>Contribute</Button></a>
+            <a onClick={() => scrollToComponent(this.Team, { offset: -70, align: 'top', duration: 1500})}><Button>Team</Button></a>
+          </div>
+
+          <div class="rightNav">
+            <i class="material-icons">account_box</i>
+
+            <div class="myAccountBox">
+              <div class="address"> Address: 0x47d8e19Ab54CED9d4Afc7C5519C6481C3D893d8d </div>
+              <div class="eth"> ETH: 12 </div>
+              <div class="dctoken"> My DC: 1223 </div>
+            </div>
+          </div>
+
+        </nav>
+
+        <div id="startDiv"> <Start/> </div>
+        <div ref={(section) => { this.About = section; }}><About/></div>
+        <div ref={(section) => { this.Whitepaper = section; }}> <Whitepaper/> </div>
+        <div ref={(section) => { this.Roadmap = section; }}> <Roadmap/> </div>
+        <div ref={(section) => { this.Contribute = section; }}> <Contribute/> </div>
+        <div ref={(section) => { this.Team = section; }}> <Team/> </div>
+
       </div>
     );
   }
